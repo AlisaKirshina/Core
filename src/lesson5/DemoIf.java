b67eb3ff07bb4aa14d834aad8eaa65524de751a7
79130 <alice.kirshina@yandex.ru>
2022-10-10 15:26:05 +0700

    add new print

---
+++ src/lesson5/DemoIf.java
@@ -0,0 +1,14 @@
+package lesson5;
+
+public class DemoIf {
+    public static void main (String[]args) {
+        System.out.println(args[0]);
+        int number = Integer.parseInt(args[0]);
+        if (number % 2 != 0) {
+            System.out.println(number);
+        }
+        System.out.println("Урра!");
+
+    }
+
+}
